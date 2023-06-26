07e43249e013d6088e58379a41f2b137596afe4d
Flloki97 <fbryma7@gmail.com>
2023-06-26 15:44:15 +0200

    update in JS

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+const section_1 = document.getElementById("vertical");
+const col_left = document.querySelector(".col_left");
+const timeln = gsap.timeline({paused: true});
+
+timeln.fromTo(col_left, {y: 0}, {y: '170vh', duration: 1, ease: 'none'}, 0);
+
+const scroll_1 = ScrollTrigger.create({
+    animation: timeln,
+    trigger: section_1,
+    start: 'top top',
+    end: 'bottom center',
+    scrub: true
+});
