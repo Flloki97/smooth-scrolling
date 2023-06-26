d2d8a7f59d498a24a4cd6342b4e9b6ead6e64804
Flloki97 <fbryma7@gmail.com>
2023-06-26 16:24:00 +0200

    left-side-move with gsap

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const lenis = new Lenis({
 
 function raf(time) {
     lenis.raf(time);
+    ScrollTrigger.update();
     requestAnimationFrame(raf);
 }
 
@@ -14,7 +15,7 @@ const section_1 = document.getElementById("vertical");
 const col_left = document.querySelector(".col_left");
 const timeln = gsap.timeline({paused: true});
 
-timeln.fromTo(col_left, {y: 0}, {y: '170vh', duration: 1, ease: 'none'}, 0);
+timeln.fromTo(col_left, {y: 0}, {y: '190vh', duration: 1, ease: 'none'}, 0);
 
 const scroll_1 = ScrollTrigger.create({
     animation: timeln,
